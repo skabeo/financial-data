8ac3d5599046c8bf76fbb7f837835b10f94dab2f
skabeo <spencerokyere95@gmail.com>
2023-05-21 19:40:32 +0100

    Set temp country array

--- src/components/Detailspage.js
+++ src/components/Detailspage.js
@@ -6,8 +6,22 @@ import { useParams } from 'react-router-dom'
 const Detailspage = () => {
   const dispatch = useDispatch()
   const param = useParams();
+  const [country, setCountry] = useState([]);
+
   const countries = useSelector(state => state.covid.countries.filter((country) => country.continent === param.country_name));
   const countryData = useSelector(state => state.covid.countryDetails);
+
+  useEffect(() => {
+    setCountry(countries[0].countries);
+  }, []);
+
+  useEffect(() => {
+    if(country.length > 0) {
+      country.forEach((local) => {
+        dispatch(fetchCountryDetails(local));
+      })
+    }
+  }, [country])
 
   return (
     <div>
